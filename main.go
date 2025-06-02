59e224f6b6b9828c18d10758a99fe5033ec19f31
Muhammad Rizky Ardiansah <rizkyardiansah.dev@gmail.com>
2025-06-02 15:05:58 +0700

    fix: update CORS configuration

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
+		AllowOrigins:     "http://localhost:5173, http://localhost:3000",
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
